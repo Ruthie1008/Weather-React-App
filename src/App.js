1ca93230f08414042a04129a8eb78f6e6041d6a7
Ruthie1008 <116093781+Ruthie1008@users.noreply.github.com>
2023-01-05 06:32:41 -0600

    started npm and imported code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App-header">
       <h1>Weather App</h1>
+      <Weather />
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+import "./styles.css";
+
+export default function Weather() {
+  let [city, setCity] = useState("");
+  let [forecast, setForecast] = useState("");
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    let apiKey = "c819171fe0abdc14039af4ef5dda283b";
+    let units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+    axios.get(apiUrl).then(showForecast);
+    function showForecast(response) {
+      console.log(response);
+      setForecast(
+        <ul className="forecast">
+          <li>Temperature: {Math.round(response.data.main.temp)}°C</li>
+          <li>Description: {response.data.weather[0].description}</li>
+          <li>Humidity: {response.data.main.humidity}%</li>
+          <li>Wind: {response.data.wind.speed}km/h</li>
+          <li>
+            <img
+              src={`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`}
+              alt={response.data.weather[0].description}
+            />
+          </li>
+        </ul>
+      );
+    }
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Enter a city..."
+          onChange={updateCity}
+        />
+        <input type="submit" value="Search" />
+      </form>
+      <div>{forecast}</div>
+    </div>
+  );
+}
